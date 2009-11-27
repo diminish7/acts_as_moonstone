4d6775e65a221b0a8050c56303746e00dbf22ef8
Jason Rush <jasonrush@jason-rushs-macbook-pro.local>
2009-11-27 12:10:26 -0700

    added id and class to index

--- lib/engine.rb
+++ lib/engine.rb
@@ -4,13 +4,16 @@ module Acts
       
       #Creates a Lucene document from an ActiveRecord object
       def doc_from(record)
-        if record.respond_to?(:indexables)
-          doc = Lucene::Document::Doc.new
-          record.indexables.each do |indexable|
-            doc.add_field(indexable.to_s, record.send(indexable))
-          end
-          doc
+        raise "Can't index an unsaved record" if record.new_record?
+        raise "Can't index a record that does not act as moonstone" unless record.respond_to?(:indexables)
+        doc = Lucene::Document::Doc.new
+        record.indexables.each do |indexable|
+          value = record.send(indexable)
+          puts "#{indexable} (#{indexable.class}) --> #{value} (#{value.class})"
+          doc.add_field(indexable.to_s, value.to_s) unless value.nil?
         end
+        doc.add_field("class", record.class.to_s)
+        doc
       end
       
     end
--- spec/engine_test.rb
+++ spec/engine_test.rb
@@ -5,12 +5,16 @@ describe Acts::Moonstone::Engine do
   it "should create a Lucene Document from an ActiveRecord object" do
     engine = Acts::Moonstone::Engine.new
     company = Company.new(:name => "Awesome, Inc")
+    company.save.should be_true
     contact = Contact.new(:first_name => "Jason", :last_name => "Rush", :company => company)
+    contact.save.should be_true
     doc = engine.doc_from(contact)
+    doc["id"].should == contact.id.to_s
     doc["first_name"].should == "Jason"
     doc["last_name"].should == "Rush"
     doc["full_name"].should == "Jason Rush"
     doc["company_name"].should == "Awesome, Inc"
+    doc["class"].should == "Contact"
   end
   
 end
