c49ee9173ff6cd163278d84b624d036668bf974d
Jason Rush <jasonrush@jason-rushs-macbook-pro.local>
2009-11-26 18:24:52 -0700

    fixed yaml loading

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), "lib", "acts_as_moonstone")
 
-Acts::Moonstone::CONFIG = File.join(RAILS_ROOT, "config", "moonstone_config.yml")
-Acts::Moonstone::INDEX = Acts::Moonstone::CONFIG[:path_to_index] || File.join(RAILS_ROOT, "index")
+Acts::Moonstone::CONFIG = YAML.load_file(File.join(RAILS_ROOT, "config", "moonstone_config.yml"))
+Acts::Moonstone::INDEX = Acts::Moonstone::CONFIG["path_to_index"] || File.join(RAILS_ROOT, "index")
 
 class ActiveRecord::Base
   extend Acts::Moonstone
