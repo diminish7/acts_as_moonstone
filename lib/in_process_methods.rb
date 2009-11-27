05bc50a96e09dec3ec3f2eb042f8350b14be0858
Jason Rush <jasonrush@jason-rushs-macbook-pro.local>
2009-11-27 12:12:58 -0700

    stubbed out the indexing API

--- lib/in_process_methods.rb
+++ lib/in_process_methods.rb
@@ -1,10 +1,24 @@
 module Acts
   module Moonstone
     
-    module InProcessInstanceMethods  
+    module InProcessInstanceMethods
+      
+      def add_to_index
+      end
+      
+      def remove_from_index
+      end
+      
+      def update_in_index
+      end
+      
     end
     
     module InProcessClassMethods
+      
+      def reindex
+      end
+      
     end
     
   end
--- lib/restful_methods.rb
+++ lib/restful_methods.rb
@@ -2,9 +2,23 @@ module Acts
   module Moonstone
     
     module RestfulInstanceMethods  
+      
+      def add_to_index
+      end
+      
+      def remove_from_index
+      end
+      
+      def update_in_index
+      end
+      
     end
     
     module RestulfClassMethods
+      
+      def reindex
+      end
+      
     end
     
   end
